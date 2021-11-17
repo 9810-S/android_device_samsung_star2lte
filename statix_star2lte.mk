a6a3dd65716bbe87d070a0b13e74234b15828172
Dylan Neve <dylanneve1@gmail.com>
2021-11-17 14:05:28 +0000

    Statix: Update for StatixOS

--- statix_star2lte.mk
+++ statix_star2lte.mk
@@ -23,16 +23,17 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 $(call inherit-product, device/samsung/star2lte/device.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common AOSP stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
-TARGET_INCLUDE_PIXEL_CHARGER := true
+# Inherit some common StatixOS stuff.
+$(call inherit-product, vendor/statix/config/common.mk)
+$(call inherit-product, vendor/statix/config/gsm.mk)
+
 TARGET_GAPPS_ARCH := arm64
 FACE_UNLOCK_SUPPORTED := true
 TARGET_INCLUDE_LIVE_WALLPAPERS := true
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := star2lte
-PRODUCT_NAME := aosp_star2lte
+PRODUCT_NAME := statix_star2lte
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-G965F
 PRODUCT_MANUFACTURER := samsung
